f09336160b9a2326e55ec47ad2b9205e1ea6a7d2
MarkMutt <lleasy8@gmail.com>
2024-12-29 15:55:37 -0500

    add env files

--- apps/staff-app/app/firestore/dbconnection.ts
+++ apps/staff-app/app/firestore/dbconnection.ts
@@ -1,5 +1,19 @@
 import { initializeFirestoreFoodPantryDb } from "@mono-dash/food-pantry-db";
+import { getServerEnv } from "../env.server";
 
+const {  SERVICE_ACCOUNT, FIREBASE_APP_NAME } =
+  getServerEnv();
 
+const { foodPantryDb } = initializeFirestoreFoodPantryDb({
+  FIREBASE_APP_NAME,
+  SERVICE_ACCOUNT,
+  collectionPaths:{
+    users:"users",
+    applications:"applications",
+    events:"events",
+    registrations:"registrations",
+    reservations:"reservations",
+  }
+});
 
-const firestore = initializeFirestoreFoodPantryDb({})
+export { foodPantryDb}
